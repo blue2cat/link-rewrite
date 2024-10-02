3319b04dffc03057bcb854f01a2d5515609e69a2
Gavin <46635587+blue2cat@users.noreply.github.com>
2024-10-01 21:06:53 -0700

    Initial push

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { config } from 'dotenv';
 
+config();
+
+// Define the Env type used to gather the redirects list
+export interface Env {
+	JSON_URL: string;
+}
+
+// Main/default action
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		let redirects;
+
+		try {
+
+			// Fetch the list and check the results
+			const response = await fetch(env.JSON_URL);
+			if (!response.ok) {
+				throw new Error('Failed to load redirects');
+			}
+
+			redirects = response.json();
+
+		} catch (error) {
+			console.error(error);
+			return new Response('Error loading redirects', { status: 500 });
+		}
+
+		// Check the redirect mappings
+		for (const redirect of redirects) {
+			if (url.pathname === redirect.uri) {
+				return Response.redirect(redirect.target, 301);
+			}
+		}
+
+		// Default response for other requests
+		return new Response('Invalid', { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
